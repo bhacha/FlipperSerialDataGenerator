0a8b16491e1469c940a9f1c870c61854ab3b09a1
Brandon Hacha <29590546+bhacha@users.noreply.github.com>
2025-08-30 16:54:13 -0500

    added some comments

--- serial_reader.py
+++ serial_reader.py
@@ -2,9 +2,9 @@ import serial
 import time
 
 ser = serial.Serial("COM5", 230400, timeout=0)
-ser.write('log \r'.encode('utf-8'))
+ser.write('log \r'.encode('utf-8')) #This puts Flipper into log mode, where it prints the log to console (serial)
     
 while True:
-    time.sleep(.1)
-    s = ser.readline().decode()       
-    print(str(s[-6:-1]).replace("m", ''))
+    time.sleep(.1) # Just for aesthetics
+    s = ser.readline().decode() # Get this as a string
+    print(str(s[-6:-1]).replace("m", '')) #There's a bunch of ANSI style terms in the output. Regex would be better here, but this is simpler for me.
